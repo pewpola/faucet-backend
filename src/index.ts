9520a96c8a207e153de5d8a944841d6d247ed7e3
EDUARDO Jorge Andrade Mourao Oliveira B901599 <b901599@bnb.gov.br>
2024-05-22 14:50:04 -0300

    testing mintAndTransfer at post route

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import { mintAndTransfer } from './Web3Provider.ts'; 
 import morgan from 'morgan';
 
 const PORT = parseInt(`${process.env.PORT || 3001}`);
@@ -11,7 +12,12 @@ const app = express();
 app.use(morgan("tiny"));
 
 app.post("/mint/:wallet", async (req:Request, res: Response, next: NextFunction) => {
-    res.json(true);
+    try {
+        const tx = await mintAndTransfer(req.params.wallet);
+        res.json(tx);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 app.listen(PORT, () => console.log("Server is listening at " + PORT));
