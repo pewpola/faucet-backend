67cf263a68b7a215c78541038e64eb9a419c7242
EDUARDO Jorge Andrade Mourao Oliveira B901599 <b901599@bnb.gov.br>
2024-05-27 15:05:52 -0300

    adding user control

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,23 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN || "*"
 }));
 
+const nextMint = new Map<string, number>();
+
 app.post("/mint/:wallet", async (req:Request, res: Response, next: NextFunction) => {
+    const wallet = req.params.wallet;
+    
+    if(nextMint.has(wallet) && nextMint.get(wallet)! > Date.now())
+        return res.status(400).json(`Try again tomorrow.`);
+
     try {
-        const tx = await mintAndTransfer(req.params.wallet);
+        const tx = await mintAndTransfer(wallet);
         res.json(tx);
     } catch (error) {
         console.log(error);
         res.status(500).json(error);
     }
+
+    nextMint.set(wallet, Date.now() + (1000 * 60 * 60 * 24));
 });
 
 app.listen(PORT, () => console.log("Server is listening at " + PORT));
